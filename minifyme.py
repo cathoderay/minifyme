d52cea3ee4ed2e00ca3cb17244757bb9684c99c2
Ronald Kaiser <raios.catodicos@gmail.com>
2011-11-19 20:08:17 -0200

    making tests pass, although with duplicated code! (green light)

--- minifyme.py
+++ minifyme.py
@@ -41,9 +41,49 @@ def remove_line_comments(input):
         output += char
     return output
 
+#XXX: duplicated code! 
+def remove_multiline_comments(input):
+    output = "" 
+    inside_string = False
+    string_delimiter = ''
+    inside_comment = False
+    not_output = False
+
+    #XXX: perhaps use stack?
+    for index, char in enumerate(input):
+        if not_output:
+            not_output = False
+            continue
+
+        if inside_comment:
+            if char == '*' and index + 1 < len(input) and input[index + 1] == '/':
+                inside_comment = False
+                not_output = True
+            continue
+
+        if (not inside_string) and (char == "'" or char == '"'):
+            inside_string = True
+            string_delimiter = char
+            output += char
+            continue
+        
+        if (inside_string) and (char == string_delimiter):
+            inside_string = False
+            output += char
+            continue
+
+        if (not inside_string) and (char == '/') and \
+           (index + 1 < len(input)) and (input[index + 1] == '*'):
+            inside_comment = True
+            continue
+        output += char
+    return output
+
 
 def minifyme(input):
-   return compose([remove_line_comments, remove_line_feeds], input)
+   return compose([remove_multiline_comments, \
+                   remove_line_comments, \
+                   remove_line_feeds], input)
 
 
 if __name__ == "__main__":
