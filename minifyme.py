b7b415219e40d5d7a364bac354b03fdb8f39720a
Ronald Kaiser <raios.catodicos@gmail.com>
2011-11-19 12:50:09 -0200

    adding // removing

--- minifyme.py
+++ minifyme.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 
 import sys
+import re
+
+
+def removeLineFeeds(input):
+    return input.replace("\n", "")
+
+
+def removeLineComments(input):
+    return '\n'.join([re.sub(r'(.*)//(.*)', '\\1', line) for line in input.split('\n')])
 
 
 def minifyme(input):
-   return input.replace("\n", "")
+   return removeLineFeeds(removeLineComments(input))
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -13,6 +13,18 @@ function() {
         output = minifyme.minifyme(input)
         self.assertTrue(output.count('\n') == 0)
 
+    def testremoveSlashSlashComments(self):
+        input = """
+//my wonderful comment
+function() {
+    //i'm inside my wonderful function
+    var x = 1;
+}"""
+        output = minifyme.minifyme(input)
+        self.assertTrue(output.count('/') == 0)
+        self.assertTrue(output.count('\n') == 0)
+
+    #TODO: test // in strings
 
 if __name__ == "__main__":
     unittest.main()
