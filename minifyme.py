f23bf0288a727157e5e8742d524a3d5eff154a90
Ronald Kaiser <raios.catodicos@gmail.com>
2011-11-19 19:02:43 -0200

    passing tests, removing comments more robust

--- minifyme.py
+++ minifyme.py
@@ -11,7 +11,35 @@ def remove_line_feeds(input):
 
 
 def remove_line_comments(input):
-    return '\n'.join([re.sub(r'(.*)//(.*)', '\\1', line) for line in input.split('\n')])
+    output = "" 
+    inside_string = False
+    string_delimiter = ''
+    inside_comment = False
+
+    #XXX: perhaps use stack?
+    for index, char in enumerate(input):
+        if inside_comment:
+            if char == '\n':
+                inside_comment = False
+                output += char
+            continue
+
+        if not inside_string and (char == '\'' or char == '"'):
+            inside_string = True
+            string_delimiter = char
+            output += char
+            continue
+        
+        if inside_string and char == string_delimiter:
+            inside_string = False
+            output += char
+            continue
+
+        if not inside_string and char == '/' and index + 1 < len(input) and input[index + 1] == '/':
+            inside_comment = True
+            continue
+        output += char
+    return output
 
 
 def minifyme(input):
