47846b9aeefda07024c80a7c1421aa1a6cca93cd
Ronald Kaiser <raios.catodicos@gmail.com>
2011-11-22 01:49:05 -0200

    changing tests names style from camelcase to _

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import minifyme
 
 class Minifyme(unittest.TestCase):
     #line feeds tests
-    def testRemovingLineFeeds(self):
+    def test_removing_line_feeds(self):
         input = r'''
         function a() {
             var x = 1;
@@ -15,44 +15,44 @@ class Minifyme(unittest.TestCase):
         output = minifyme.remove_line_feeds(input)
         self.assertEqual(0, output.count('\n'))
 
-
     #line comments tests
-    def testRemovingSlashSlashComments(self):
+    def test_removing_line_comments(self):
         input = r'''
-        //my wonderful comment
+        //first comment
         function a() {
-            //i'm inside my wonderful function
+            //second comment
             var x = 1;
         }'''
         output = minifyme.remove_line_comments(input)
         self.assertEqual(0, output.count('/'))
+	self.assertTrue(output.find('first comment') < 0)
+	self.assertTrue(output.find('second comment') < 0)
 
-    def testCantRemoveSlashSlashInsideStrings(self):
+    def test_cant_remove_double_slash_inside_strings(self):
         input = r'var x = "//foo" //bar;'
         output = minifyme.remove_line_comments(input)
         self.assertEqual(2, output.count(r'/'))
         self.assertTrue(output.find(r"//bar") < 0)
         self.assertTrue(output.find(r"//foo") > 0)
 
-    def testCantRemoveSlashSlashInsideRegex(self):
+    def test_cant_remove_double_slash_inside_regex(self):
         input = r'var x = /^\/\//;'
         output = minifyme.remove_line_comments(input)
         self.assertEqual(4, output.count(r'/'))
         self.assertTrue(output.find(r'/^\/\//;') > 0)
 
-    def testScapesAreInterpretedInsideStrings(self):
+    def test_scapes_are_interpreted_inside_strings(self):
         input = r'var a = "bla\"test//!";'
         output = minifyme.remove_line_comments(input)
         self.assertTrue(output.find(r"a\"test//") > 0)
 
-    def testCantRemoveMultilinecommentWhenRemovingLineComments(self):
+    def test_cant_remove_multiline_comment_when_removing_line_comments(self):
         input = r'/* test*/'
         output = minifyme.remove_line_comments(input)
         self.assertTrue(output.find(r'/* test*/') >= 0)
 
-
     #remove multiline comments
-    def testRemovingMultilineComments(self):
+    def test_removing_multiline_comments(self):
         input = r'''
 	/*
 	    A mind once  
@@ -69,24 +69,23 @@ class Minifyme(unittest.TestCase):
         self.assertTrue(output.find(r'*/') < 0)
         self.assertTrue(output.find(r'var') > 0)
 
-    def testCantRemoveFakeMultilineCommentsInsideStrings(self):
+    def test_cant_remove_fake_multiline_comments_inside_strings(self):
         input = r'var a = "/*asdf*/";'
         output = minifyme.remove_multiline_comments(input)
         self.assertTrue(output.find(r'"/*asdf*/"') > 0)
     
-    def testCantRemoveFakeMultilineCommentsInsideRegex(self):
+    def test_cant_remove_fake_multiline_comments_inside_regex(self):
         input = r'var a = /\/*asdf*\//;'
         output = minifyme.remove_multiline_comments(input)
         self.assertTrue(output.find(r'/*asdf*\/') > 0)
 
-    def testCantRemoveLineCommentWhenRemovingMultilineComments(self):
+    def test_cant_remove_line_comment_when_removing_multiline_comments(self):
         input = r'// /*asdf' 
         output = minifyme.remove_multiline_comments(input)
         self.assertTrue(output.find(r'// /*asdf') >= 0)
 
-
     #remove leading and trailing whitespaces
-    def testRemovingLeadingAndTrailingWhitespaces(self):
+    def test_removing_leading_and_trailing_whitespaces(self):
         input = r'''
         function() {  
 	        var x = 1;   
